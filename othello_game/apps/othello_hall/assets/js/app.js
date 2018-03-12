3db4990ba546c28a8a00669779e95fde154bbc16
Sibendu Dey <sibendu.dey@gmail.com>
2018-03-12 17:56:54 -0400

    app.js and package.json commit

--- othello_game/apps/othello_hall/assets/js/app.js
+++ othello_game/apps/othello_hall/assets/js/app.js
@@ -18,3 +18,20 @@ import "phoenix_html"
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 import socket from "./socket"
+
+import React                    from 'react';
+import ReactDOM                 from 'react-dom';
+import { browserHistory }       from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore           from './store';
+import Root                     from './container/root';
+
+
+const store  = configureStore(browserHistory);
+const history = syncHistoryWithStore(browserHistory, store);
+
+const target = document.getElementById('main_container');
+const node = <Root routerHistory={history} store={store}/>;
+
+ReactDOM.render(node, target);
+
