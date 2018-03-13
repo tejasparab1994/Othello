6276f64cb752d995b681b63d9f2d5dd93accacda
Sibendu Dey <sibendu.dey@gmail.com>
2018-03-13 18:08:22 -0400

    Created Lobby channel

--- othello_game/apps/othello_hall/lib/othello_hall_web/channels/lobby_channel.ex
+++ othello_game/apps/othello_hall/lib/othello_hall_web/channels/lobby_channel.ex
@@ -1,6 +1,8 @@
 defmodule OthelloHallWeb.LobbyChannel do
   use OthelloHallWeb, :channel
 
+  alias Othello.GameSupervisor
+
   def join("lobby:join", payload, socket) do
     if authorized?(payload) do
       {:ok, socket}
@@ -12,13 +14,16 @@ defmodule OthelloHallWeb.LobbyChannel do
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
   def handle_in("lobby:current_games", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+    IO.puts "Request for current games"
+    {:reply, {:ok, %{}}, socket}
   end
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
   def handle_in("lobby:new_game", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+    game_names = GameSupervisor.game_names
+    IO.puts game_names
+    {:reply, {:ok, game_names}, socket}
   end
 
   # It is also common to receive messages from the client and
